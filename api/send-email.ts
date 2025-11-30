13d75e9518c276ef736e9a8844c633ca40f53d95
rykrook <ryan.krook@outlook.com>
2025-11-30 22:42:08 +0100

    -

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,5 +1,12 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+}
 export default async function handler(req: any, res: any) {
-
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   //cors
   if (req.method === 'OPTIONS') {
     res.status(200).end()
