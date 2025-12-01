62e252bf50739c648aa86006bb1a8ac3e6be90e1
rykrook <ryan.krook@outlook.com>
2025-12-01 01:10:54 +0100

    test

--- client/api/send-email.ts
+++ client/api/send-email.ts
@@ -23,6 +23,7 @@ export default async function handler(req: any, res: any) {
         service_id: process.env.EMAILJS_SERVICE_ID,
         template_id: process.env.EMAILJS_TEMPLATE_ID,
         user_id: process.env.EMAILJS_PUBLIC_KEY,
+        private_key: process.env.EMAILJS_PRIVATE_KEY,
         template_params: {
           name: name || 'Okänt namn',
           email: email || 'ingen@email.se',
